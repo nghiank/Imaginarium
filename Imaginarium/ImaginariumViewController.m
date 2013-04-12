5471ec5f0a2be2ab8445f63c27c85557df9b626e
nghia <nghia@chiena-imac.ads.autodesk.com>
2013-04-12 18:40:16 +0800

    update

--- Imaginarium/ImaginariumViewController.m
+++ Imaginarium/ImaginariumViewController.m
@@ -8,16 +8,39 @@
 
 #import "ImaginariumViewController.h"
 
-@interface ImaginariumViewController ()
-
+@interface ImaginariumViewController()
+@property (nonatomic, retain) UIImageView *oneselement;
 @end
 
 @implementation ImaginariumViewController
 
+@synthesize oneselement = _oneselement;
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    //initialize your UIImageView
+    UIImageView *newOneselement = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ee.jpg"]];
+    
+    
+    UIImage *image = newOneselement.image;
+    self.oneselement = newOneselement;
+    self.oneselement.userInteractionEnabled = YES;
+    self.oneselement.contentMode = UIViewContentModeTopLeft;
+    
+    
+    CGRect rect =CGRectMake(0, 0, image.size.width, image.size.height);
+    self.oneselement.frame = rect;
+    
+    CGRect smallRect = CGRectMake(0, 0, 500, 500);
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:smallRect];
+    scrollView.contentSize = CGSizeMake(image.size.width, image.size.height);
+    [scrollView addSubview:self.oneselement];
+    [self.view addSubview:scrollView];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +48,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
